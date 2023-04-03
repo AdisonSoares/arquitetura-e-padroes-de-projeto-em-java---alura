bced178c0ed15d585d0c9028bf4edb3bbbec242e
adison <adison.soares7@gmail.com>
2023-04-03 20:59:45 -0300

    Design_Patterns1

---
+++ Design_Patterns1/src/br/com/alura/loja/TestesPedidos.java
@@ -0,0 +1,23 @@
+package br.com.alura.loja;
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+import br.com.alura.loja.pedido.*;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
+
+public class TestesPedidos {
+	public static void main(String[] args) {
+		String cliente = "Alvaro";
+		BigDecimal valorOrcamento = new BigDecimal("300");
+		int quantidadeItens = Integer.parseInt("2");
+		
+		GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
+		GeraPedidoHandler handler = new GeraPedidoHandler(
+				Arrays.asList(new SalvarPedidoNoBancoDeDados(),
+						new EnviarEmailPedido())); 
+		handler.executa(gerador);
+	
+	}
+
+}
